3ac7105a1c992533c335b91126a6f4cc20355bf5
Tejasnavadkar <tejas.navadkar@gmail.com>
2025-06-21 11:01:48 +0530

    add port in env

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const rootRouter = require("./routes/index")
 const app = express()
 const cors = require("cors")
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json()); // bodyparser middleware
 
